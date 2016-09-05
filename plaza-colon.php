8f7b8041a9c095ca98d299d1efaceaa92cd76bc0
PedroBorundaMixen <pedro.borunda@mixen.mx>
2016-09-05 16:23:01 -0600

    Plaza Colon Modificacion

--- plaza-colon.php
+++ plaza-colon.php
@@ -1,46 +1,136 @@
 <?php $thispage="america-latina" ?>
 <?php include ('header.php'); ?>
 <div class="wrapper desarrollo colon">
-	<div id="carousel-fraccionamiento" class="carousel slide" data-ride="carousel">
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
 
-		<!-- Wrapper for slides -->
-		<div class="carousel-inner" role="listbox">
-			<div class="item active">
-				<img class="no-drag" src="img/banner/bg-colon.jpg" alt="Banner">
-			</div>
-			<div class="item">
-				<img class="no-drag" src="img/banner/colon.jpg" alt="Banner">
-			</div>
-		</div>
+    <!--/* BANNER */-->
 
-		<!-- Controls -->
-		<a class="left carousel-control" href="#carousel-fraccionamiento" role="button" data-slide="prev">
-			<span class="sr-only">Previous</span>
-		</a>
-		<a class="right carousel-control" href="#carousel-fraccionamiento" role="button" data-slide="next">
-			<span class="sr-only">Next</span>
-		</a>
-	</div>
-	<!-- Carousel -->
-	<div class="description">
-		<div class="container spacing">
-			<div class="col-sm-6">
-				<h1 class="header blue text-center">NOVEDOSA PLAZA COMERCIAL</h1>
-				<p class="text">
-					En el centro de la ciudad, ubicada en la intersección
-					de avenidas principales, albergará locales y oficinas comerciales, contando con amplio
-					estacionamiento. Siendo así un agradable espacio de intercambio social, esperala muy pronto.
-					<h3 class="text-center preventa">Preventa: Abril 2016</h3>
-				</p>
-			</div>
-			<div class="col-sm-6"></div>
-			<img class="hidden-xs" src="img/colon/table.png">
-		</div>
-	</div>
-	<div class="prox">
-		<div class="container text-center transform-center-vertical">
-			<h1 class="white">¡espérala muy pronto!</h1>
-		</div>
-	</div>
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <section class="banner" id="banner">
+        <div id="carousel-fraccionamiento" class="carousel slide" data-ride="carousel">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img class="no-drag" src="img/banner/bg-colon.jpg" alt="Banner">
+                </div>
+                <div class="item">
+                    <img class="no-drag" src="img/banner/colon.jpg" alt="Banner">
+                </div>
+            </div>
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-fraccionamiento" role="button" data-slide="prev">
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-fraccionamiento" role="button" data-slide="next">
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
+    <!-- Carousel -->
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+
+    <!--/* DESCRIPTION */-->
+
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <section class="description" id="description">
+        <div class="description">
+            <div class="container spacing">
+                <div class="col-sm-6">
+                    <h1 class="header blue text-left">TU SITIO DE <span class="bold">ENCUENTRO</span></h1>
+                    <p class="text">
+                        Ubicada entre  Av. Colón y Niños Héroes de la Ciudad de 
+                        Chihuahua, contará con más de 111 cajones de estacionamiento. 
+                        Su ubicación estratégica es una salida natural del Centro de la 
+                        Ciudad hacia vialidades importantes. Siendo así la mejor
+                        plaza de la ciudad.
+                    </p>
+                </div>
+                <div class="col-sm-6"></div>
+                <img class="hidden-xs transform-center-vertical" src="img/colon/tienda.png">
+            </div>
+        </div>
+    </section>
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+
+    <!--/* ANALISIS */-->
+
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <section class="analisis" id="analisis">
+        <div class="analisis">
+            <div class="container spacing">
+                <h1 class="header blue text-left">ANÁLISIS <span class="bold">SOCIODEMOGRÁFICO</span></h1>
+                <p class="line"></p>
+                <row class="aforo" id="aforo">
+                    <div class="col-sm-4">
+                        <div class="img-container">
+                            <img class="transform-center-vertical" src="img/colon/icons/auto.png" alt="">
+                        </div>
+                        <h4>AFORO VEHICULAR</h4>
+                        <h2>2,850</h2>
+                        <p>vehículos/hora</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="img-container">
+                            <img class="transform-center-vertical" src="img/colon/icons/casa.png" alt="">
+                        </div>
+                        <h4>AFORO HABITACIONAL</h4>
+                        <h2>12,500</h2>
+                        <p>hogares en área de influencia</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="img-container">
+                            <img class="transform-center-vertical" src="img/colon/icons/personas.png" alt="">
+                        </div>
+                        <h4>AFORO PEATONAL</h4>
+                        <h2>175</h2>
+                        <p>personas/hora</p>
+                    </div>
+                </row>
+            </div>
+        </div>
+    </section>
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+
+    <!--/* DISTINCION */-->
+
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <section class="distincion" id="distincion">
+        <div class="banner">
+            <img src="img/colon/distincion-banner.png" alt="">
+        </div>
+        <div class="content">
+            <h1 class="transform-center-vertical">DISTINCIÓN A TU ALCANCE</h1>
+        </div>
+    </section>
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+
+    <!--/* OPCIONES */-->
+
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <section class="opciones" id="opciones">
+        <div class="opciones">
+            <div class="container spacing">
+                <div class="col-sm-6">
+                    <h1 class="header blue text-left">MÁS OPCIONES EN <span class="bold">UN SOLO LUGAR</span></h1>
+                    <ul type="disk">
+                        <li>ACABADOS DE PRIMERA CALIDAD.</li>
+                        <li>AMPLIO ESTACIONAMIENTO.</li>
+                        <li>INMEJORABLE UBICACIÓN.</li>
+                    </ul>
+                </div>
+                <div class="col-sm-6"></div>
+                <img class="hidden-xs transform-center-vertical" src="img/colon/tienda2.png">
+            </div>
+        </div>
+    </section>
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+
+    <!--/* LOTIFICACION */-->
+
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <section class="lotificacion" id="lotificacion">
+        <img src="img/colon/colon-lotificacion1.png" alt="">
+        <img src="img/colon/colon-lotificacion2.png" alt="">
+    </section>
 </div>
 <?php include ('footer.php'); ?>
